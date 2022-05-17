48e73be682bdfcd264fb4406dbeb80bc36cbbd47
Fan of fish <81502024+WhyGitHubb@users.noreply.github.com>
2022-05-17 21:56:48 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,75 @@
+--FEService, such hax ik, by me @Whygithubb
+
+local Services = (function() return setmetatable({}, {
+    __index = function(self, key)
+        s = rawget(self, key)
+        if s then
+            return s
+        end
+        s = game:GetService(key)
+        rawset(self, key, s)
+        return s
+    end
+}); end)();
+
+local Create = function(Instance, Parent)
+    T = type(Instance)
+    if T == 'string' then
+        Instance = Instance.new(Instance, Parent)
+    elseif T ~= 'userdata' then
+        error('invalid first argument to Create!')
+    end
+
+    return function(Props)
+        for k, v in next, Props do
+            Instance[k] = v
+            -- TODO: signal support
+        end
+        return Instance
+    end
+end
+
+
+local FE = {}
+
+function FE:FullNetwork() --Immediately makes player have full physics capability
+    local plr = Services.Players.LocalPlayer
+    setscriptable(plr, "SimulationRadius", true)
+	setscriptable(plr, "MaximumSimulationRadius", true)
+	setscriptable(plr, "MaxSimulationRadius", true)
+    local physicSettings = settings().Physics
+    physicSettings.AllowSleep = false
+	physicSettings.PhysicsEnvironmentalThrottle = Enum['EnviromentalPhysicsThrottle'].Disabled
+    plr.MaximumSimulationRadius = 1000
+    plr.ReplicationFocus = workspace or Services.Workspace
+	plr.MaxSimulationRadius = 1000  -- no point in looping
+	plr.SimulationRadius = 1000
+end
+
+function FE:Chat(Msg)
+    local Msg = tostring(Msg)
+    if not Msg then return end
+    Services.Players:Chat(Msg) --such fe hax
+end
+
+function FE:GrabTools()
+    for i,v in next, Services.Workspace:GetChildren() do
+        if v:IsA("BackpackItem") then
+            v:EquipTool()
+        end
+    end
+end
+
+function FE:ClaimPart(Part)
+    
+end
+
+function FE:UnclaimPart(Part)
+
+end
+
+function FE:SetHumanoidAnimationSpeed(Speed)
+
+end
+
+return FE
