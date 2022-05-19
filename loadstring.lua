563eaeda31dd4197ff4ff503be7f969acd90fe16
Fan of fish <81502024+WhyGitHubb@users.noreply.github.com>
2022-05-19 12:24:00 +0800

    Create loadstring.lua
    usage

---
+++ loadstring.lua
@@ -0,0 +1,8 @@
+--usage
+
+local FEService = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/WhyGitHubb/FEService/main/main.lua"))()
+
+
+FEService:FullNetwork()  
+
+print(game.Players.LocalPlayer.SimulationRadius)
